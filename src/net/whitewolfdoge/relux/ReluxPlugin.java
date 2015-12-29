5c329c8314e2fa522e5b70b98738d9e60389be5a
WhiteWolfdoge <emjwhite@outlook.com>
2015-12-29 14:45:49 -0800

    Added bells but no whistles

--- src/net/whitewolfdoge/relux/ReluxPlugin.java
+++ src/net/whitewolfdoge/relux/ReluxPlugin.java
@@ -1,7 +1,44 @@
 package net.whitewolfdoge.relux;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ReluxPlugin extends JavaPlugin{
-
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("relux")){ // If the command was "relux" or one of its aliases
+			if(!sender.hasPermission("relux.use")){ // If the command sender does not have permission
+				sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
+				return true;
+			}
+			else if(args.length < 1){ // If there was no argument
+				return false;
+			}
+			else if(args.length > 1){ // If there was more than one argument
+				return false;
+			}
+			else{
+				return processRequest(sender, args[0]);
+			}
+		}
+		else{ // Command not recognized by this plugin
+			return false;
+		}
+	}
+	
+	private boolean processRequest(CommandSender sender, String radRaw){
+		try{
+			byte rad = Byte.parseByte(radRaw);
+			
+			sender.sendMessage(sender.getName());
+			
+			//TODO next
+			return true;
+		}
+		catch(NumberFormatException nfe){ // The radius wans't a valid number
+			return false;
+		}
+	}
 }
