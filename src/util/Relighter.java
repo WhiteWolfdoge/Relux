230f4438bc4e51ccc783f5518880d82467968104
WhiteWolfdoge <emjwhite@outlook.com>
2016-01-18 01:11:47 -0800

    Added support for unloaded chunks and removed native refrencing

--- src/util/Relighter.java
+++ src/util/Relighter.java
@@ -1,7 +1,9 @@
 package util;
 
+/**
 import net.minecraft.server.v1_8_R3.BlockPosition;
 import net.minecraft.server.v1_8_R3.World;
+**/
 
 import org.bukkit.Chunk;
 import org.bukkit.block.Block;
@@ -14,11 +16,15 @@ public class Relighter{
 	 * @param Chunk chk		The chunk to be relighted
 	 */
 	public static void relightChunk(Chunk chk){
-		for(byte xLoc = 0; xLoc <= 15; xLoc++){ // X axis
-			for(byte yLoc = (byte)255; yLoc >= 0; yLoc--){ // Y axis
-				for(byte zLoc = 0; zLoc <= 15; zLoc++){ // Z axis
-					Block blk = 	chk.getBlock(xLoc, yLoc, zLoc); // The block at the current xyz
-					relightBlock(blk);
+		if(!chk.isLoaded()){ // IF the chunk is NOT loaded
+			chk.load(); // Try to load it
+		}
+		if(chk.isLoaded()){ // If the chunk is loaded now
+			for(byte xLoc = 0; xLoc <= 15; xLoc++){ // For every block on the X axis
+				for(byte yLoc = 0; yLoc <= 255; yLoc++){ // For every block on the Y axis
+					for(byte zLoc = 0; zLoc <= 15; zLoc++){ // For every block on the Z axis
+						relightBlock(chk.getBlock(xLoc, yLoc, xLoc)); // Relight the block
+					}
 				}
 			}
 		}
@@ -29,11 +35,15 @@ public class Relighter{
 	 * @param Block blk		The block to be relighted
 	 */
 	public static void relightBlock(Block blk){
+		blk.getState().update(true);
+		
+		/**
 		World wld =			(World)blk.getWorld();
 		int xLoc =			blk.getX();
 		int yLoc =			blk.getY();
 		int zLoc = 			blk.getZ();
 		BlockPosition bp =	new BlockPosition(xLoc, yLoc, zLoc);
 		wld.x(bp); // Magic!
+		**/
 	}
 }
