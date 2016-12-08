54df723f6aed1976cf3c6e6c879e3b09d38e8c95
WhiteWolfdoge <emjwhite@outlook.com>
2016-12-07 16:40:41 -0800

    Added catching missing natives

--- src/net/whiteWolfdoge/relux/util/Native.java
+++ src/net/whiteWolfdoge/relux/util/Native.java
@@ -1,6 +1,9 @@
 package net.whiteWolfdoge.relux.util;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
+
+import net.whiteWolfdoge.relux.ReluxPlugin;
 
 class Native{
 	
@@ -27,16 +30,16 @@ class Native{
 		@formatter:on
 	 */
 	protected static boolean relightBlock(int inBlockX, int inBlockY, int inBlockZ, World inBlockWorld){
-		
 		try{ // NMS v1_9_R1
+			Class.forName("net.minecraft.server.v1_9_R1.WorldServer", false, Class.class.getClassLoader()); // Probe an NMS native
 			net.minecraft.server.v1_9_R1.BlockPosition blkPos = new net.minecraft.server.v1_9_R1.BlockPosition(inBlockX, inBlockY, inBlockZ);
 			
 			org.bukkit.craftbukkit.v1_9_R1.CraftWorld craftWld = (org.bukkit.craftbukkit.v1_9_R1.CraftWorld)inBlockWorld;
 			net.minecraft.server.v1_9_R1.WorldServer worldSrv = craftWld.getHandle();
 			return worldSrv.w(blkPos); // Magic!
 		}
-		catch(Exception ex){
-			ex.printStackTrace();
+		catch(ClassNotFoundException cnfex){
+			Bukkit.getLogger().warning(ReluxPlugin.MSG_EX_PREFIX + "Natives were not found, try updating");
 		}
 		
 		return false;
