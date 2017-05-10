33d6d52d7966a7d7fd20cf30dfa9a33e84784fc8
WhiteWolfdoge <emjwhite@outlook.com>
2017-05-09 22:24:48 -0700

    Rewrote RelightTask to be an instantiable object

--- src/net/whiteWolfdoge/relux/task/RelightTask.java
+++ src/net/whiteWolfdoge/relux/task/RelightTask.java
@@ -1,5 +1,29 @@
 package net.whiteWolfdoge.relux.task;
 
+import java.util.Arrays;
+
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
 public class RelightTask{
-	//	
+	private int blkTotal;
+	private World wld;
+	private Block[] blkList;
+	
+	/**
+	 * Constructor to restrict access
+	 */
+	@SuppressWarnings("unused")
+	private RelightTask(){
+		// Do nothing
+	}
+	
+	public RelightTask(Block[] newBlkList){
+		if(newBlkList == null || newBlkList.length == 0){ // Verify valid array
+			throw new IllegalArgumentException("A RelightTask must contain at least one block.");
+		}
+		blkTotal = newBlkList.length;
+		wld = newBlkList[0].getWorld();
+		blkList = Arrays.copyOf(newBlkList, newBlkList.length); // Take a clone of the array
+	}
 }
