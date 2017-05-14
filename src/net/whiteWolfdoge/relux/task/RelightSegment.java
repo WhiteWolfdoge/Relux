1d6b00e8860864089c944a3006ca6d11dd666992
WhiteWolfdoge <emjwhite@outlook.com>
2017-05-13 22:10:44 -0700

    Constructor for making a new relight segment

--- src/net/whiteWolfdoge/relux/task/RelightSegment.java
+++ src/net/whiteWolfdoge/relux/task/RelightSegment.java
@@ -1,7 +1,21 @@
 package net.whiteWolfdoge.relux.task;
 
+
 /**
  * A RelightSegment is a tool to relight a series of blocks, taking some basic statistics.
  */
 public class RelightSegment{
+	/**
+	 * Constructor to limit default constructor
+	 */
+	private RelightSegment(){
+		// Do nothing
+	}
+	
+	/**
+	 * This constructor allows you to crate a new RelightSegment using a list of blocks.
+	 */
+	public RelightSegment(Block[] newBlkList){
+		// TODO
+	}
 }
