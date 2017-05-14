f16363b270039947fe9d9fa174a796b98c8853e2
WhiteWolfdoge <emjwhite@outlook.com>
2017-05-13 20:24:58 -0700

    Added doc, fixed ex message

--- src/net/whiteWolfdoge/relux/task/RelightRegion.java
+++ src/net/whiteWolfdoge/relux/task/RelightRegion.java
@@ -5,10 +5,14 @@ import java.util.Arrays;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 
+/**
+ * A RelightRegion is a collection of blocks to be used for relighting.
+ */
 public class RelightRegion{
 	private int blkTotal;
 	private World wld;
 	private Block[] blkList;
+	private boolean completed;
 	
 	/**
 	 * Constructor to restrict access
@@ -20,10 +24,13 @@ public class RelightRegion{
 	
 	public RelightRegion(Block[] newBlkList){
 		if(newBlkList == null || newBlkList.length == 0){ // Verify valid array
-			throw new IllegalArgumentException("A RelightTask must contain at least one block.");
+			throw new IllegalArgumentException("A RelightRegion must contain at least one block.");
 		}
+		
 		blkTotal = newBlkList.length;
 		wld = newBlkList[0].getWorld();
 		blkList = Arrays.copyOf(newBlkList, newBlkList.length); // Take a clone of the array
+		
+		completed = false;
 	}
 }
